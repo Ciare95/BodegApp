e5dbea44031674772ec0c443afc3feb6808f5256
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2026-03-23 14:54:27 -0500

    envolver la variable con comillas en el .bat

--- iniciar.bat
+++ iniciar.bat
@@ -1,8 +1,8 @@
 @echo off
-set BASE=C:\Users\WIN 10\BodegApp
+set "BASE=C:\Users\WIN 10\BodegApp"
 
 :: Backend
-start "Backend" cmd /k "%BASE%\venv\Scripts\activate && python %BASE%\manage.py runserver 0.0.0.0:8000"
+start "Backend" cmd /k ""%BASE%\venv\Scripts\activate" && python "%BASE%\manage.py" runserver 0.0.0.0:8000"
 
 :: Frontend
-start "Frontend" cmd /k "cd /d %BASE%\frontend && npm run preview"
+start "Frontend" cmd /k "cd /d "%BASE%\frontend" && npm run preview"
